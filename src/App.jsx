52fda18548091f5b02a92df28cf53d8c63ed080b
kgeta <kgeta@localhost>
2026-04-11 15:41:57 +0100

    all package installed, ready to go

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <p className='bg-gray-600'>KSB E-Gallery</p>
+    </>
+  );
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').config} */
+export default {
+  content: ["./**/*.{html,js,jsx,ts,tsx}"],
+  // content: ["index.html" ,"./src/**/*.{js,ts,jsx,tsx}" ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
